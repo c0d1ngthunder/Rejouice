ef2542953cb1664bab73098c1565603a3c972666
Nitish Kumar <codingthunder3@gmail.com>
2025-04-28 14:59:34 +0530

    feat: Add smooth cursor transition effect for better UX

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const cursor = document.querySelector('#cursor');
-const page1 = document.querySelector('#page1');
+const cursor = document.querySelector("#cursor");
+const page1 = document.querySelector("#page1");
 
-page1.addEventListener('mousemove', (e) => {
-  const x = e.clientX;
-  const y = e.clientY;
+page1.addEventListener("mouseenter", (e) => {
+  cursor.style.opacity = 1;
+})
+
+window.addEventListener("mousemove", (e) => {
+  const x = e.pageX;
+  const y = e.pageY;
 
   cursor.style.top = `${y}px`;
   cursor.style.left = `${x}px`;
+});
+
+page1.addEventListener("mouseleave", (e) => {
+  cursor.style.opacity = 0;
 })
