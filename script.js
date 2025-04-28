4a55784b656ca4b3818c2bd84ff3b2e0c2f6e370
Nitish Kumar <codingthunder3@gmail.com>
2025-04-28 18:21:22 +0530

    feat: Add page 2 with text animations

--- script.js
+++ script.js
@@ -1,18 +1,80 @@
-const cursor = document.querySelector("#cursor");
-const page1 = document.querySelector("#page1");
+gsap.registerPlugin(ScrollTrigger);
 
-page1.addEventListener("mouseenter", (e) => {
-  cursor.style.opacity = 1;
-})
+function customCursor() {
+  const cursor = document.querySelector("#cursor");
+  const page1 = document.querySelector("#page1");
 
-window.addEventListener("mousemove", (e) => {
-  const x = e.pageX;
-  const y = e.pageY;
+  page1.addEventListener("mouseenter", (e) => {
+    gsap.to(cursor, {
+      opacity: 1,
+      duration: 0.2,
+      ease: "linear",
+    });
+  });
 
-  cursor.style.top = `${y}px`;
-  cursor.style.left = `${x}px`;
-});
+  window.addEventListener("mousemove", (e) => {
+    const x = e.pageX;
+    const y = e.pageY;
 
-page1.addEventListener("mouseleave", (e) => {
-  cursor.style.opacity = 0;
-})
+    gsap.to(cursor, {
+      x: x,
+      y: y,
+      duration: 0.2,
+      ease: "linear",
+    });
+  });
+
+  page1.addEventListener("mouseleave", (e) => {
+    gsap.to(cursor, {
+      opacity: 0,
+      duration: 0.2,
+      ease: "linear",
+    });
+  });
+}
+
+function page2textanimation() {
+  const text = document.querySelector(".page2-text");
+  const textArray = text.textContent.split(" ");
+  text.innerHTML = ""; // Clear the text content
+  textArray.forEach((word, index) => {
+    if (word.trim() !== "") {
+      const wrapper = document.createElement("span");
+      wrapper.classList.add("wrapper");
+
+      const span = document.createElement("span");
+      span.classList.add("word");
+      span.textContent = word;
+
+      wrapper.appendChild(span);
+
+      text.appendChild(wrapper);
+    }
+  });
+
+  gsap.from(".head", {
+    y: 100,
+    duration: 0.5,
+    scrollTrigger: {
+      trigger: "#page2-head",
+      start: "top 80%",
+      end: "bottom 20%",
+      markers: false,
+    },
+    stagger: 0.01,
+  });
+
+  gsap.from(".word", {
+    y: 120,
+    duration: 0.5,
+    scrollTrigger: {
+      trigger: "#page2-content",
+      start: "top 50%",
+      end: "bottom 20%"
+    },
+    stagger: 0.01,
+  });
+}
+
+page2textanimation();
+customCursor();
